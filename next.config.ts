034742dce022fa7590f677d3938b5b17d5818b57
Ola Fischer <ola.fischer85@gmail.com>
2026-08-06 19:25:21 +0200

    Migrate Fameko to OpenNext

--- next.config.ts
+++ next.config.ts
@@ -3,3 +3,4 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {};
 
 export default nextConfig;
+import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
